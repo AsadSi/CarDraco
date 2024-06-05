1526fed2d2974073b738bd30866669c6918333ad
Asad Siddiqi <asad@wecircle.io>
2024-06-05 22:57:41 +0200

    a

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
@@ -7,63 +7,70 @@ const Login = () => {
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/cars');
+    }
+  }, [navigate]);
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-        const response = await fetch('https://apicedraco20240522123857.azurewebsites.net/api/User/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
+      const response = await fetch('https://apicedraco20240522123857.azurewebsites.net/api/User/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-        if (response.ok) {
-            const data = await response.json();
-            const { token, role } = data;
+      if (response.ok) {
+        const data = await response.json();
+        const { token, role } = data;
 
-            localStorage.setItem('token', token);
-            localStorage.setItem('role', role.toString()); 
-            setError(null);
-            navigate('/cars');
-        } else {
-            setError('Invalid credentials. Please try again.');
-        }
+        localStorage.setItem('token', token);
+        localStorage.setItem('role', role.toString()); 
+        setError(null);
+        navigate('/cars');
+      } else {
+        setError('Invalid credentials. Please try again.');
+      }
     } catch (error) {
-        console.error('Login error:', error.message);
-        setError('An error occurred during login. Please try again.');
+      console.error('Login error:', error.message);
+      setError('An error occurred during login. Please try again.');
     }
   };
 
   return (
     <div className="form-container">
-        <div>
+      <div>
         <h2>Login</h2>
         <form onSubmit={handleLogin}>
-            <div className="form-group">
-                <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                />
-            </div>
-            <div className="form-group">
-                <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                />
-            </div>
-            <button type="submit">Login</button>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <p>Don't have an account? <Link to="/signup">signup</Link></p>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <p>Don't have an account? <Link to="/signup">signup</Link></p>
         </form>
-        </div>
+      </div>
     </div>
   );
 };
