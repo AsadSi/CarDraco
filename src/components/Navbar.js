1ac4c05eb3361c8c615509714f9210a8a0b7e186
Asad Siddiqi <asad@wecircle.io>
2024-06-06 00:13:57 +0200

    we made it this time right?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,6 @@ import './style/navbar.css';
 const Navbar = () => {
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
-  const token = localStorage.getItem('token');
-
 
   const handleLogout = () => {
     localStorage.removeItem('token');
@@ -20,7 +18,7 @@ const Navbar = () => {
         <li><Link to="/cars">Our Cars</Link></li>
         <li><Link to="/sellcar">Sell Your Car</Link></li>
         <li><Link to="/mycars">My Cars</Link></li>
-        {token && <li><button onClick={handleLogout}>Logout</button></li>}
+        <li><button onClick={handleLogout}>Logout</button></li>
       </ul>
       <div className="menu-icon" onClick={() => setShowMenu(!showMenu)}>
         <div className="menu-line"></div>
